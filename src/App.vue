fbd1fccfcb6d58eca5d16645c2281910e820c390
Loctd <loc.tranduy33@gmail.com>
2023-03-23 23:20:37 +0700

    Fix bug search

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
             class="search-bar" 
             placeholder="Search..."
             v-model="query"
-            @keyup.enter="fetchWeather() & loadMap()"
+            @keyup.enter="fetchWeather()"
           />
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
@@ -103,10 +103,12 @@ export default {
   },
   methods: {
     async fetchWeather () {
-      let response = await fetch(`${this.url_base}?q=${this.query === "" ? this.old_query : this.query}&units=${this.temp_c ? "metric": "imperial"}&appid=${this.api_key}`)
-      let json = await response.json();
-      this.weather = await json;
-
+        let response = await fetch(`${this.url_base}?q=${this.query === "" ? this.old_query : this.query}&units=${this.temp_c ? "metric": "imperial"}&appid=${this.api_key}`)
+        
+        if(response.ok) {
+          let json = await response.json();
+          this.weather = await json;
+        }
     },
     dateBuilder () {
       let d = new Date();
